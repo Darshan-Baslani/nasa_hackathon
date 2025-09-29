6613bd88f5956d9d27cae0737f894002043440a0
Darshan-Baslani <darshan.baslani4@gmail.com>
2025-09-29 19:54:45 +0530

    add: forecast api

--- controllers/aqi.py
+++ controllers/aqi.py
@@ -25,3 +25,23 @@ async def calculate_aqi(latitude: float, longitude: float):
 
     aqi = data["data"]["aqi"]
     return aqi
+
+
+async def forecast_aqi(latitude: float, longitude: float):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            f"http://api.waqi.info/feed/geo:{latitude};{longitude}/?token={WAQI_API_KEY}"
+        )
+    try:
+        data = response.json()
+    except Exception:
+        raise ValueError(f"Non-JSON response: {response.text[:200]}")
+
+    if isinstance(data['data'], str):
+        print(data['data'])
+        data['data'] = json.loads(data['data'])
+    if isinstance(data['data']['forecast'], str):
+        data['data']['forecast'] = json.loads(data['data']['aqi'])
+
+    aqi = data["data"]["forecast"]
+    return aqi
--- routers/aqi.py
+++ routers/aqi.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from controllers.aqi import calculate_aqi
+from controllers.aqi import calculate_aqi, forecast_aqi
 
 router = APIRouter()
 
@@ -12,3 +12,8 @@ class AQI(BaseModel):
 async def get_aqi(request: AQI):
     aqi = await calculate_aqi(request.latitude, request.longitude)
     return {"aqi": aqi}
+
+@router.get("/forecast-aqi")
+async def get_forecast_aqi(request: AQI):
+    forecast = await forecast_aqi(request.latitude, request.longitude)
+    return {"forecast": forecast}
